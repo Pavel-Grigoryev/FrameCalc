499ecf615fdc3e412d992d5afafa61192004de1b
Pavel <3935371@gmail.com>
2023-03-30 16:22:02 +0300

    feat(cart): cartBuble has been added

--- src/features/Cart/CartIcon/CartIcon.tsx
+++ src/features/Cart/CartIcon/CartIcon.tsx
@@ -3,20 +3,27 @@ import IconButton from '@mui/material/IconButton';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import {useActions} from '../../../hooks/useAction';
 import {appActions} from '../../../app';
+import s from './CartIcon.module.css';
+import {useAppSelector} from '../../../hooks/useAppSelector';
+import {cartSelectors} from '../index';
 
 export const CartIcon = () => {
 
     const {openCart} = useActions(appActions);
 
+    const items = useAppSelector(cartSelectors.selectItems);
 
     const cartOpenHandler = () => {
         openCart(true);
     };
 
     return (
-        <div >
+        <div className={s.cartBlock}>
+            {!!items.length && (<div className={s.cartBuble}>
+                <span>{items.length}</span>
+            </div>)}
             <IconButton onClick={cartOpenHandler} sx={{color: '#FFF'}}>
-                <ShoppingCartOutlinedIcon />
+                <ShoppingCartOutlinedIcon/>
             </IconButton>
         </div>
     );
